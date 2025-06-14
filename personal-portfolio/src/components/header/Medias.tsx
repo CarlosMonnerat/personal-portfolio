213785b66adcfdf7f3c6eab98c78f282e5e3fc2c
carlos <carlosh.monnerat@gmail.com>
2025-06-14 18:41:37 -0300

    update social medias

--- personal-portfolio/src/components/header/Medias.tsx
+++ personal-portfolio/src/components/header/Medias.tsx
@@ -1,13 +1,25 @@
 import Link from "next/link";
+import Image from "next/image";
+import linkedin from "../../../public/assets/img/icon-linkedin.svg";
+import github from "../../../public/assets/img/icon-github.svg";
+
 
 export default function Medias() {
    return (
       <div className="flex flex-row gap-10">
-         <Link href='#'>
-            LinkedIN
+         <Link href='#' className="bg-zinc-300 w-8 h-8 rounded-full">
+            <Image 
+               src ={ linkedin }
+               alt="Logotipo do LinkedIn"
+               // width={35}
+               // height={35}
+            />
          </Link>
-         <Link href='#'>
-            GitHub
+         <Link href='#' className="bg-zinc-300 w-8 h-8 rounded-full">
+            <Image
+               src={ github }
+               alt="logotipo do GitHub"
+            />
          </Link>
       </div>
    )
