9c745011a4ad95f2fb4de1a724690f2b0d1dfbde
carlos <carlosh.monnerat@gmail.com>
2025-06-14 20:48:36 -0300

    Update Nav.tsx

--- personal-portfolio/src/components/header/Nav.tsx
+++ personal-portfolio/src/components/header/Nav.tsx
@@ -4,7 +4,7 @@ export default function Nav() {
    return (
       <nav>
          <ul className="flex flex-row gap-10 font-bold text-white m-2">
-            <Link href='#' className="hover:text-green-300">
+            <Link href='#' className="hover:text-cyan-300">
                <li>Sobre Mim</li>
             </Link>
             <Link href='#'>
