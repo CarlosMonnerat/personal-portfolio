43b32c6dfca3153010df0dadb8e25ca1db4de5ce
carlos <carlosh.monnerat@gmail.com>
2025-06-14 20:01:54 -0300

    Create background video

--- personal-portfolio/src/app/page.tsx
+++ personal-portfolio/src/app/page.tsx
@@ -1,9 +1,11 @@
+import Background from "@/components/background/Background";
 import Header from "@/components/header/Header";
 
 export default function Home() {
   return (
     <div>
       <Header/>
+      <Background/>
     </div>
   );
 }
