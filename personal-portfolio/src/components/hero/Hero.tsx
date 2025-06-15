39d93f812533655a82b621a9cf24c953894c2ba5
carlos <carlosh.monnerat@gmail.com>
2025-06-15 14:57:45 -0300

    update hero

--- personal-portfolio/src/components/hero/Hero.tsx
+++ personal-portfolio/src/components/hero/Hero.tsx
@@ -1,17 +1,18 @@
 import Description from "./description"
+import HeroImg from "./HeroImg"
 import Title from "./Titulo"
 import Welcome from "./Welcome"
 
 export default function Hero() {
    return (
-      <section className="flex flex-row justify-around items-center gap-10 w-full bg-zinc-600">
+      <section className="flex flex-row justify-around items-center gap-10 w-full bg-transparent">
          <div className="flex flex-col gap-3">
             <Welcome/>
             <Title/>
             <Description/>
          </div>
          <div>
-            imagem
+            <HeroImg/>
          </div>
       </section>
    )      
---
+++ personal-portfolio/src/components/hero/HeroImg.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import Image from "next/image";
+import logo from "../../../public/assets/img/hero-img.svg"
+
+export default function HeroImg() {
+   return (
+         <Image
+         className="flex justify-center items-center w-lg"
+            src={ logo }
+            alt="Imagem da hero do portifólio"
+            width={150}
+         />   
+   )
+};
