9d9857c3e422a59de22935d8e9b4cbf9435f5a81
carlos <carlosh.monnerat@gmail.com>
2025-06-15 12:37:44 -0300

    Update Medias.tsx

--- personal-portfolio/src/components/header/Medias.tsx
+++ personal-portfolio/src/components/header/Medias.tsx
@@ -7,18 +7,21 @@ import github from "../../../public/assets/img/icon-github.svg";
 export default function Medias() {
    return (
       <div className="flex flex-row gap-10">
-         <Link href='#' target="_blank" 
-            className="bg-zinc-300 w-8 h-8 rounded-full hover:bg-purple-400">
-            <Image 
-               src ={ linkedin }
-               alt="Logotipo do LinkedIn"
-            />
+         <Link href='#'
+            target="_blank"
+            className="bg-zinc-300 w-8 h-8 rounded-full hover:border-3 border-purple-400" >
+               <Image
+                  src ={ linkedin }
+                  alt="Logotipo do LinkedIn"
+               />
          </Link>
-         <Link href='https://github.com/CarlosMonnerat' target="_blank" className="bg-zinc-300 w-8 h-8 rounded-full">
-            <Image
-               src={ github }
-               alt="logotipo do GitHub"
-            />
+         <Link href='https://github.com/CarlosMonnerat'
+            target="_blank"
+            className="bg-zinc-300 w-8 h-8 rounded-full hover:border-3 border-purple-400">
+               <Image
+                  src={ github }
+                  alt="logotipo do GitHub"
+               />
          </Link>
       </div>
    )
