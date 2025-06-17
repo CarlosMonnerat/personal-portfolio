36f5f46fd181f4840ca23b5e9a0db35b617ff7cf
carlos <carlosh.monnerat@gmail.com>
2025-06-16 22:32:53 -0300

    Update Welcome.tsx

--- personal-portfolio/src/components/hero/Welcome.tsx
+++ personal-portfolio/src/components/hero/Welcome.tsx
@@ -1,6 +1,6 @@
 export default function Welcome() {
    return (
-      <div className="flex justify-center items-center w-60 text-white bg-gradient-to-r from-purple-600 to-purple-950 px-1 py-1 mb-10 border-1 border-white">
+      <div className="flex justify-center items-center w-60 text-white bg-gradient-to-r from-purple-600 to-purple-950 px-1 py-1 mb-10 border-1 border-white select-none">
          Bem-vindo(a) ao meu Portifólio
       </div>
    )
