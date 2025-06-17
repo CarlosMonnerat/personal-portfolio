7260240643cfa639b34d62cc5b9626b3ddccfc6a
carlos <carlosh.monnerat@gmail.com>
2025-06-16 22:26:41 -0300

    into slides section

--- personal-portfolio/src/app/page.tsx
+++ personal-portfolio/src/app/page.tsx
@@ -15,6 +15,9 @@ export default function Home() {
       <Header/>
       <main className="relative z-10 text-white px-8 py-20 space-y-20">
         <Hero/>
+        <h2 className="flex justify-center items-center mt-60 text-white text-3xl font-bold">
+            Meus Projetos
+        </h2>
         <Projects/>
       </main> 
     </>
--- personal-portfolio/src/components/projects/Projects.tsx
+++ personal-portfolio/src/components/projects/Projects.tsx
@@ -1,17 +1,9 @@
 export default function Projects() {
    return (
-      <section className="flex flex-col justify-around items-center mt-52 w-full z-20 bg-green-500">
-         <h2 className="flex items-center text-white text-3xl font-bold">
-            Meus Projetos
-         </h2>
-         <div className="flex items-start max-w-xl text-white font-light">
-            <p>
-               Landing Pages
-            </p>
-         </div>
-         
-         
-         
+      <section className="flex flex-col items-center gap-5 w-full z-20 bg-green-500">
+            <p>Slides Landing Pages</p>
+            <p>Slides APIs e Games</p>
+            <p>Slides React Next: E-Commerce</p>
       </section>
    )      
 };
