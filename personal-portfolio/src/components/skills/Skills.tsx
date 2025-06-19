7e02b15d0bb926cbb21a4080d078aad92e0aa9aa
carlos <carlosh.monnerat@gmail.com>
2025-06-19 18:21:17 -0300

    Update Skills.tsx

--- personal-portfolio/src/components/skills/Skills.tsx
+++ personal-portfolio/src/components/skills/Skills.tsx
@@ -22,7 +22,7 @@ const skills: Skill[] = [
 
 export default function Skills() {
   return (
-    <div className="flex flex-row justify-around items-center flex-wrap gap-10 p-4 w-full max-w-6xl mx-auto rounded-xl bg-transparent">
+    <div className="flex flex-row justify-around items-center flex-wrap gap-10 p-4 w-full max-w-3xl mx-auto rounded-xl bg-transparent">
       {skills.map((skill, index) => (
         <div key={index} className="relative group">
           <Image
