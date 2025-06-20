f8a116f8e6a05504773ffaee4f6bc52735d6e3df
carlos <carlosh.monnerat@gmail.com>
2025-06-20 19:39:54 -0300

    Update About.tsx

--- personal-portfolio/src/components/about/About.tsx
+++ personal-portfolio/src/components/about/About.tsx
@@ -1,18 +1,32 @@
+import Image from "next/image";
+import perfil from "../../../public/assets/img/homem.png"
 import CurriculoSection from "./CurriculoSection";
 
 export default function About() {
    return (
-      <section className="flex flex-row justify-between items-start gap-10 p-4 w-full max-w-7xl mx-auto rounded-xl border-1 border-purple-600 shadow-[0_0_30px_8px_rgba(192,132,252,0.5)]">
-         <video
-            className="w-xl h-full opacity-20 object-cover mix-blend-soft-light"
-            autoPlay
-            loop
-            muted
-            playsInline
-         >
-            <source src="/assets/videos/earth.mp4" type="video/mp4" />
-               Seu navegador não suporta o vídeo.
-         </video>
+      <section className="flex flex-row justify-evenly items-start gap-10 p-4 w-full max-w-7xl mx-auto rounded-xl border-1 border-purple-600 shadow-[0_0_30px_8px_rgba(192,132,252,0.5)]">
+         <div className="flex flex-col justify-evenly items-center gap-10">
+            <div>
+               <Image
+                  src={ perfil }
+                  alt="Imagem da logo provisória"
+                  width={280}
+                  className="border-5 border-purple-950 rounded-full opacity-75 mt-8"
+
+               />
+            </div>
+            <video
+               className="max-w-sm h-full opacity-20 object-cover mix-blend-soft-light"
+               autoPlay
+               loop
+               muted
+               playsInline
+            >
+               <source src="/assets/videos/earth.mp4" type="video/mp4" />
+                  Seu navegador não suporta o vídeo.
+            </video>
+         </div>
+        
 
          <div className="flex flex-col justify-start items-center gap-2 w-xl h-full">
             <h3 className="text-3xl font-extrabold uppercase p-4 mb-8 text-purple-600">Carlos Henrique Monnerat Quintanilha</h3>
