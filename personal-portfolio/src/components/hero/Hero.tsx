52f0f3e080c40de0b30a499cc1e72f2330122b64
carlos <carlosh.monnerat@gmail.com>
2025-07-08 20:44:44 -0300

    update responsividade

--- personal-portfolio/src/components/hero/Hero.tsx
+++ personal-portfolio/src/components/hero/Hero.tsx
@@ -1,19 +1,19 @@
-import Description from "./description"
-import HeroImg from "./HeroImg"
-import Title from "./Titulo"
-import Welcome from "./Welcome"
+import Description from "./description";
+import HeroImg from "./HeroImg";
+import Title from "./Titulo";
+import Welcome from "./Welcome";
 
 export default function Hero() {
-   return (
-      <section className="flex flex-row flex-1 justify-around items-center w-full z-20 bg-transparent">
-         <div className="flex flex-col gap-3">
-            <Welcome/>
-            <Title/>
-            <Description/>
-         </div>
-         <div>
-            <HeroImg/>
-         </div>
-      </section>
-   )      
-};
+  return (
+    <section className="flex flex-row flex-1 justify-around items-center w-full z-20 bg-transparent hero-responsive">
+      <div className="flex flex-col gap-3">
+        <Welcome />
+        <Title />
+        <Description />
+      </div>
+      <div>
+        <HeroImg />
+      </div>
+    </section>
+  );
+}
