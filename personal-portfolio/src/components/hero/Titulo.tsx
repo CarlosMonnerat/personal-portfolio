57f23a95ef8b38561142dd7ec0f9dc85e040e5dd
carlos <carlosh.monnerat@gmail.com>
2025-07-08 19:34:54 -0300

    Update Titulo.tsx

--- personal-portfolio/src/components/hero/Titulo.tsx
+++ personal-portfolio/src/components/hero/Titulo.tsx
@@ -2,7 +2,7 @@ export default function Title() {
   return (
     <h1 className="flex flex-col max-w-xl text-white text-6xl font-bold bg-transparent leading-tight">
       <span>
-        Olá, eu sou <span className="text-gradient animated-gradient-reverse">Carlos</span>
+        Olá, eu sou <span className="text-gradient animated-gradient-reverse text-7xl">Carlos</span>
       </span>
       <span className="animated-gradient-reverse">
         <span className="typing-wrapper">
