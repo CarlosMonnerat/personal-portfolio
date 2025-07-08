8da79db538a9e930e0ee1ec657fc3e8f684631a8
carlos <carlosh.monnerat@gmail.com>
2025-07-08 19:57:54 -0300

    update media title

--- personal-portfolio/src/components/hero/Titulo.tsx
+++ personal-portfolio/src/components/hero/Titulo.tsx
@@ -1,12 +1,17 @@
 export default function Title() {
   return (
-    <h1 className="flex flex-col max-w-xl text-white text-6xl font-bold bg-transparent leading-tight">
+    <h1 className="flex flex-col max-w-xl text-white text-6xl font-bold bg-transparent leading-tight title-h1">
       <span>
-        Olá, eu sou <span className="text-gradient animated-gradient-reverse text-7xl">Carlos</span>
+        Olá, eu sou{" "}
+        <span className="text-gradient animated-gradient-reverse text-7xl title-carlos">
+          Carlos
+        </span>
       </span>
       <span className="animated-gradient-reverse">
         <span className="typing-wrapper">
-          <span className="text-gradient text-7xl">Monnerat</span>
+          <span className="text-gradient text-7xl title-monnerat">
+            Monnerat
+          </span>
         </span>
       </span>
     </h1>
