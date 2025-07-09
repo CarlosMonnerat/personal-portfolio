e9c81f79eac5572701c1b9712abe63aec5055cc7
carlos <carlosh.monnerat@gmail.com>
2025-07-09 13:59:27 -0300

    create queriesContact.css

--- personal-portfolio/src/components/contact/ContactSection.tsx
+++ personal-portfolio/src/components/contact/ContactSection.tsx
@@ -2,18 +2,18 @@ import FormComponent from "./formComponent";
 
 export default function ContactSection() {
   return (
-    <section className="flex flex-row justify-evenly items-start max-w-7xl mx-auto rounded-xl">
+    <section className="flex flex-row justify-evenly items-start max-w-7xl mx-auto rounded-xl contact-section">
       <video
-            className="w-xl h-auto opacity-20 object-cover mix-blend-soft-light"
-            autoPlay
-            loop
-            muted
-            playsInline
-         >
-            <source src="/assets/videos/about.mp4" type="video/mp4" />
-               Seu navegador não suporta o vídeo.
-         </video>
-      <FormComponent/>
+        className="w-xl h-auto opacity-20 object-cover mix-blend-soft-light contact-video"
+        autoPlay
+        loop
+        muted
+        playsInline
+      >
+        <source src="/assets/videos/about.mp4" type="video/mp4" />
+        Seu navegador não suporta o vídeo.
+      </video>
+      <FormComponent />
     </section>
-  )
-};
+  );
+}
