c6f2e0767945dd7e956748e12bc32c977fbd8f84
carlos <carlosh.monnerat@gmail.com>
2025-07-09 14:15:26 -0300

    Update Footer.tsx

--- personal-portfolio/src/components/footer/Footer.tsx
+++ personal-portfolio/src/components/footer/Footer.tsx
@@ -1,13 +1,21 @@
 // components/Footer.tsx
-
-import Logo from "../header/logo";
+import Link from "next/link";
+import Image from "next/image";
+import logo from "../../../public/assets/img/logo_portfolio.svg";
 import Medias from "../header/Medias";
 
 export default function Footer() {
   return (
     <footer className="flex flex-row items-center justify-around mt-16">
-      <Logo/>
-      <p className="text-white">
+      <Link href="/" className="block logo-small w-full is-desktop">
+        <Image
+          src={logo}
+          alt="Imagem da logo"
+          className="w-full h-auto object-contain"
+          priority
+        />
+      </Link>
+      <p className="text-white p-10">
          © Portifólio criado para fins profissionais | Carlos Monnerat - {new Date().getFullYear()}
       </p>
       <Medias/>
