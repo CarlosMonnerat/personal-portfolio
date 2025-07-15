3aac0ac6f354464fad0ef4421fdbf1ed1d7d769e
carlos <carlosh.monnerat@gmail.com>
2025-07-15 16:09:04 -0300

    Update VideoList.ts

--- personal-portfolio/src/components/projects/VideoList.ts
+++ personal-portfolio/src/components/projects/VideoList.ts
@@ -9,21 +9,6 @@ export interface VideoItem {
 
 export const videoList: VideoItem[] = [
   {
-    file: 'project1.mp4',
-    siteUrl: 'https://meusite.com/project1',
-    githubUrl: 'https://github.com/usuario/project1',
-  },
-  {
-    file: 'project2.mp4',
-    siteUrl: 'https://meusite.com/project2',
-    githubUrl: 'https://github.com/usuario/project2',
-  },
-  {
-    file: 'project3.mp4',
-    siteUrl: 'https://meusite.com/project3',
-    githubUrl: 'https://github.com/usuario/project3',
-  },
-  {
     file: 'crud.mp4',
     siteUrl: 'https://carlosmonnerat.github.io/CRUD_Cadastro_de_Clientes/',
     githubUrl: 'https://github.com/CarlosMonnerat/CRUD_Cadastro_de_Clientes',
