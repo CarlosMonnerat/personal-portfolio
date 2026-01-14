0177f2873f67e3f206aaeb5a451e80aa923e3788
carlos <carlosh.monnerat@gmail.com>
2026-01-14 17:10:47 -0300

    Update Hero Description

--- src/components/hero/description.tsx
+++ src/components/hero/description.tsx
@@ -1,7 +1,7 @@
 export default function Description() {
    return (
       <p className="flex justify-start items-center max-w-xl text-white font-light">
-         Desenvolvedor Front-End apaixonado por criar interfaces modernas, responsivas e acessíveis. Com foco em performance e usabilidade, transformo ideias em experiências digitais envolventes usando tecnologias como HTML, CSS, JavaScript, TypeScript, React e Next.js.
+         Desenvolvedor Full Stack especializado em criar interfaces web modernas, escaláveis e responsivas, com foco em performance e experiência do usuário. Aplicações completas: arquitetura sólida, APIs e banco de dados robustos e seguros.
       </p>
    )
 };
